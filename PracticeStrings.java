0de77357ffaee81285ac7687c84c3007c77ba7c0
srishtisachdeva <76564991+srishtisachdeva@users.noreply.github.com>
2021-09-17 16:18:27 +0530

    string methods

---
+++ PracticeStrings.java
@@ -0,0 +1,27 @@
+
+public class PracticeStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Srishti";
+		System.out.println(name);
+		System.out.println("Hello," + " How are you doing!!");
+		char a = name.charAt(4);
+		System.out.println(a);
+	    System.out.println(name.charAt(2));
+	    System.out.println(name.compareToIgnoreCase("srishti"));
+	    System.out.println(name.concat(" Sachdeva"));
+	    System.out.println(name.endsWith("ti"));
+	    System.out.println(name.equals("Srishtihg"));
+	    //System.out.println(name.format()));
+	    //System.out.println(name.getChars(4, 6, 'd', 3));
+	    System.out.println(name.length());
+	    System.out.println(name.hashCode());
+	    System.out.println(name.indexOf("h"));
+	    System.out.println(name.toLowerCase());
+	    System.out.println(name.toUpperCase());
+	    
+	    
+	}
+
+}
